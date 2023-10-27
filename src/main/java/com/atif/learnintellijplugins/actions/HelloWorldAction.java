f8dca51b7afee2dd68dc6b997699037a509b8a84
Atif Saleed <atif.mohd.sld@gmail.com>
2023-10-27 15:13:05 +0530

    Action now shows selected text instead of "Hello world!"

--- src/main/java/com/atif/learnintellijplugins/actions/HelloWorldAction.java
+++ src/main/java/com/atif/learnintellijplugins/actions/HelloWorldAction.java
@@ -2,12 +2,28 @@ package com.atif.learnintellijplugins.actions;
 
 import com.intellij.openapi.actionSystem.AnAction;
 import com.intellij.openapi.actionSystem.AnActionEvent;
+import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.ui.Messages;
 import org.jetbrains.annotations.NotNull;
 
 public class HelloWorldAction extends AnAction {
     @Override
     public void actionPerformed(@NotNull AnActionEvent e) {
-        Messages.showInfoMessage("Hello world!", "Info");
+        // Get the editor from the event
+        Editor editor = e.getDataContext().getData(com.intellij.openapi.actionSystem.CommonDataKeys.EDITOR);
+
+        if (editor != null) {
+            // Get the selected text from the editor
+            String selectedText = editor.getSelectionModel().getSelectedText();
+
+            if (selectedText != null && !selectedText.isEmpty()) {
+                // Display the selected text
+                Messages.showInfoMessage("Selected Text: " + selectedText, "Selected Text Info");
+            } else {
+                Messages.showInfoMessage("No text selected.", "Info");
+            }
+        } else {
+            Messages.showInfoMessage("No editor found.", "Info");
+        }
     }
 }
