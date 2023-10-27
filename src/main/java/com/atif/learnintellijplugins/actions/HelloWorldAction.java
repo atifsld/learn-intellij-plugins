9204beefd32a212aa9645c171be24764d24d0e99
Atif Saleed <atif.mohd.sld@gmail.com>
2023-10-27 15:10:45 +0530

    Revert "Action now shows selected text instead of "Hello world!""
    This reverts commit 58c3074e607d9152f1ce0fdb7dc7d6fcc40342b0.

--- src/main/java/com/atif/learnintellijplugins/actions/HelloWorldAction.java
+++ src/main/java/com/atif/learnintellijplugins/actions/HelloWorldAction.java
@@ -6,22 +6,8 @@ import com.intellij.openapi.ui.Messages;
 import org.jetbrains.annotations.NotNull;
 
 public class HelloWorldAction extends AnAction {
+    @Override
     public void actionPerformed(@NotNull AnActionEvent e) {
-        // Get the editor from the event
-        Editor editor = e.getDataContext().getData(com.intellij.openapi.actionSystem.CommonDataKeys.EDITOR);
-
-        if (editor != null) {
-            // Get the selected text from the editor
-            String selectedText = editor.getSelectionModel().getSelectedText();
-
-            if (selectedText != null && !selectedText.isEmpty()) {
-                // Display the selected text
-                Messages.showInfoMessage("Selected Text: " + selectedText, "Selected Text Info");
-            } else {
-                Messages.showInfoMessage("No text selected.", "Info");
-            }
-        } else {
-            Messages.showInfoMessage("No editor found.", "Info");
-        }
+        Messages.showInfoMessage("Hello world!", "Info");
     }
 }
